af57d2fe9a238f48670c525be5df2aaedeec657b
Adınız <siz@e-posta.com>
2022-10-27 17:02:46 +0300

    update mklinux

--- build.sh
+++ build.sh
@@ -9,22 +9,5 @@ fi
 sed -i "s/9999/${VERSION}/g" debian/changelog
 # Stage 1: Get version and fetch source code
 # fetch source
-wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${VERSION}.tar.xz
-# extrack if directory not exists
-[[ -d linux-${VERSION} ]] || tar -xf linux-${VERSION}.tar.xz
-echo 1 > .stage
-
-
-# Enter source
-cd linux-${VERSION}
-
-# Redefine version
-#export VERSION=$(cat ../debian/changelog | head -n 1 | sed "s/.*(//g" | sed "s/).*//g")
-export VERSION=$(curl https://kernel.org/ | grep "downloadarrow_small.png" | sed "s/.*href=\"//g;s/\".*//g;s/.*linux-//g;s/\.tar.*//g")
-
-# Stage 2: Build & Install source code (Like archlinux)
-pkgdir=../debian/linux
-mkdir -p "$pkgdir"
-wget https://gitlab.com/sulinos/devel/sulin-sources/-/raw/master/mklinux -O mklinux
-chmod +x mklinux
-bash mklinux "$pkgdir"
+wget https://gitlab.com/sulix/devel/sources/mklinux/-/raw/master/mklinux.sh -O mklinux
+bash mklinux -o "$pkgdir" -t linux -c "https://gitlab.com/sulix/devel/sources/mklinux/-/raw/master/config"
